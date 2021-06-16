c641b6d6e0118c35ce9f97f0431f0f31235306bd
Filippo Biondi <filippo.biondi@protonmail.ch>
2021-06-16 20:04:12 +0200

    Implemented chip8 skeleton

---
+++ src/chip8.rs
@@ -0,0 +1,47 @@
+#[allow(dead_code)]
+pub struct Chip8 {
+    opcode: u16,
+    v: Vec<u8>,
+    i: u16,
+    pc: u16,
+    stack: Vec<u16>,
+    sp: u16,
+    ram: Vec<u8>,
+    vram: Vec<u8>,
+    redraw: bool,
+    keys: Vec<u8>,
+    delay_t: u8,
+    sound_t: u8,
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        Self {
+            opcode: 0,
+            v: vec![0; 16],
+            i: 0,
+            pc: 0x200, //rom program starts at 0x200 memory address (512 in decimal)
+            stack: vec![0; 16],
+            sp: 0,
+            ram: vec![0; 4096],
+            vram: vec![0; 32 * 64],
+            redraw: false,
+            keys: vec![0; 16],
+            delay_t: 0,
+            sound_t: 0,
+        }
+    }
+
+    pub fn clock(&mut self) {
+        self.vram[0] = 1;
+        self.redraw=true;
+    }
+
+    pub fn get_vram(&self) -> Vec<u8> {
+        self.vram.clone()
+    }
+
+    pub fn awaiting_redraw(&self) -> bool {
+        self.redraw
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,49 @@
 use std::time::{Duration, Instant};
 use std::thread::sleep;
+use macroquad::prelude::*;
 
-fn main() {
+mod chip8;
+use crate::chip8::Chip8;
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[macroquad::main("YACI")]
+async fn main() {
+    let mut system = Chip8::new(); 
+
+    set_camera(&Camera2D {
+        rotation: 0.0,
+        zoom: Vec2::new(0.03, 0.03),
+        target: Vec2::new(screen_width() / 2.0 - 32.0, screen_height() / 2.0 - 16.0),
+        offset: Vec2::new(0.0, 0.0),
+        render_target: None
+    });
+
+    let mut image = Image::gen_image_color(64, 32, BLACK);
+
+    let texture = Texture2D::from_image(&image);
+    texture.set_filter(FilterMode::Nearest);
+
     loop {
         let now = Instant::now();
-        println!("test");
-        sleep(Duration::from_millis(17u64  - now.elapsed().as_millis() as u64));
+        println!("FPS: {}", get_fps());
+        system.clock();
+        if system.awaiting_redraw() {
+            let vram = system.get_vram();
+            for i in 0..vram.len() {
+                image.set_pixel(
+                    (i % 64) as u32,
+                    (i / 64) as u32,
+                    match vram[i] {
+                        1 => WHITE,
+                        _ => BLACK,
+                    },
+                );
+            }
+            texture.update(&image);
+            draw_texture(texture, screen_width() / 2.0 - 64.0, screen_height() / 2.0 - 32.0, LIGHTGRAY);
+        }
+        sleep(Duration::from_millis(16u64  - now.elapsed().as_millis() as u64));
+        next_frame().await
     }
 }
